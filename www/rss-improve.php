411686595a6c903ef9d60c6920dd4a4857321feb
Dusan.Halicky <Dusan.Halicky@004e7a2e-e3e0-11de-8dea-4d08a2fd72f5>
2011-01-25 23:24:32 +0000

    www - added rss-improve.php

---
+++ www/rss-improve.php
@@ -0,0 +1,18 @@
+<?php
+  // rss feed of recent user's improvements
+  require_once "purin2/rss/rss.php";
+  $lang = 'sk';
+  if ($_REQUEST['lang']=='en') $lang = 'en';
+  puRssBegin("Recent user improvements - $lang","Last 20 improvements submited by users in $lang language","dusan.halicky@gmail.com","http://ayass.xf.cz/ghost/rss-improve.php");
+  $file = file_get_contents("data/$lang/improve.dat");
+  $lines = explode("\n",$file);
+  $qa = array();
+  for ($i=0; $i<count($lines) / 2 - 1; $i++) {
+    array_push($qa,'Q: '.$lines[2*$i].'<br/>A: '.$lines[2*$i+1].'<br/><br/>');
+    if ($i >= 20)
+      break;
+  }
+  $file = implode("\n",$qa);
+  puRssItem(md5($file),"New improvements","http://ayass.xf.cz/ghost/","http://ayass.xf.cz/ghost/data/$lang/improve.dat",$file);
+  puRssEnd();
+?>
