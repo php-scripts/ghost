2e5345028265cfe926909c976948bfc726cfbb1c
Dusan.Halicky <Dusan.Halicky@004e7a2e-e3e0-11de-8dea-4d08a2fd72f5>
2011-01-25 21:36:44 +0000

    www - saving improved sentences

--- trunk/www/improve-add.php
+++ trunk/www/improve-add.php
@@ -4,6 +4,23 @@
   $question = str_replace("\n"," ",htmlspecialchars(strip_tags($_REQUEST['question'])));
   $improved = str_replace("\n"," ",htmlspecialchars(strip_tags($_REQUEST['improved'])));
   
-  echo "question=$question improved=$improved";
+  // language override via cookie or param
+  if ( ($_COOKIE['lang'] == 'en')||($_REQUEST['lang'] == 'en')||($_REQUEST['lang_en'] == 'on') ) $language = 'en';
+  if ( ($_COOKIE['lang'] == 'sk')||($_REQUEST['lang'] == 'sk')||($_REQUEST['lang_sk'] == 'on') ) $language = 'sk';
+  // quess language
+  if (empty($language)) {
+    $language = 'en';
+    if (strpos(' '.$_SERVER['HTTP_ACCEPT_LANGUAGE'],'sk') > 0) 
+      $language = 'sk';
+  }   
+
+  // append question to log
+  $f = fopen("data/$ALanguage/improve.dat", "a") or die("improve: can't open file");
+  fwrite($f, $question."\n");
+  fwrite($f, $improved."\n");
+  fclose($f);  
+
+  // done
+  header("Location: index.php?lang=$language");
 
 ?>
