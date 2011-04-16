370aaaadf6a35e2ec00700ac105a391bff313988
Dusan.Halicky <Dusan.Halicky@004e7a2e-e3e0-11de-8dea-4d08a2fd72f5>
2011-04-16 12:06:20 +0000

    Backup script now save chat.txt and feedback.txt too

--- data/backup.sh
+++ data/backup.sh
@@ -5,3 +5,8 @@ find | grep -v svn | grep dat | while read i; do
     echo "http://ayass.xf.cz/ghost/data$n --> $i"
     wget -qO $i "http://ayass.xf.cz/ghost/data$n"
 done
+
+# backup also chat and feedback data
+echo "Backing up chat and feedback..."
+wget -qO ../www/chat.txt "http://ayass.xf.cz/ghost/chat.txt"
+wget -qO ../www/feedback.txt "http://ayass.xf.cz/ghost/feedback.txt"
