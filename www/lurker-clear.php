8329b6efb0bf70e124e5a85bfe14c627d3284e60
Dusan.Halicky <Dusan.Halicky@004e7a2e-e3e0-11de-8dea-4d08a2fd72f5>
2011-04-24 09:13:59 +0000

    remote wipeout for lurkers log

---
+++ www/lurker-clear.php
@@ -0,0 +1,27 @@
+<?php
+  // remote cleanup for lurker logs (so I don't need to connect via ftp)
+  require_once "purin2.php";
+  
+  // current state
+  echo "<h3><a href=\"data/en/lurker.dat\">English log</a></h3>";
+  echo "<pre>";
+  $s = file_get_contents('data/en/lurker.dat');
+  echo $s;
+  echo "(".(count(explode("\n",$s))-1)." lines total)";
+  echo "</pre>";
+  
+  echo "<h3><a href=\"data/sk/lurker.dat\">Slovak log</a></h3>";
+  echo "<pre>";
+  $s = file_get_contents('data/sk/lurker.dat');
+  echo $s;
+  echo "(".(count(explode("\n",$s))-1)." lines total)";
+  echo "</pre>";
+
+  // erase them
+  if (@$_REQUEST[$lurker_clear_password]) {
+    file_put_contents('data/en/lurker.dat','');
+    file_put_contents('data/sk/lurker.dat','');
+    echo "<h3>Logs was ERASED</h3>";
+  } else
+    echo "<h3>Logs was NOT erased</h3>";
+?>
--- www/purin2.php
+++ www/purin2.php
@@ -1,4 +1,5 @@
 <?php
     // override purin2 config file
     $pu_root = 'purin2/';
+    $lurker_clear_password = 'do-not-commit-this';
 ?>
